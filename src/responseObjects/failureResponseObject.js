44764e914c39df2e9f1aa63057f3a14b7f4c8b05
Tomasz Nowacki <tomasz.nowacki@netguru.co>
2018-12-03 20:45:27 +0100

    introduce FailureResponseObject

---
+++ src/responseObjects/failureResponseObject.js
@@ -0,0 +1,40 @@
+const { ResponseObject } = require('./responseObject');
+
+class FailureResponseObject extends ResponseObject {
+  constructor(type, message) {
+    super();
+    this.type = type;
+    this.message = message;
+  }
+
+  isSuccess() {
+    return false;
+  }
+
+  static get VALIDATION_ERROR() {
+    return 'VALIDATION_ERROR';
+  }
+
+  static get SYSTEM_ERROR() {
+    return 'SYSTEM_ERROR';
+  }
+
+  static buildSystemError(message) {
+    return new this(this.SYSTEM_ERROR, message);
+  }
+
+  static buildValidationError(message) {
+    return new this(this.VALIDATION_ERROR, message);
+  }
+
+  static buildFromInvalidRequestObject(invalidRequest) {
+    const message = invalidRequest.errors.map(({ parameter, error }) => {
+      return { parameter, error }
+    });
+    return this.buildValidationError(message);
+  }
+}
+
+module.exports = {
+  FailureResponseObject,
+};
---
+++ tests/responseObjects/failureResponseObject.test.js
@@ -0,0 +1,44 @@
+const { InvalidRequestObject } = require('../../src/requestObjects/invalidRequestObject');
+const { FailureResponseObject } = require('../../src/responseObjects/failureResponseObject');
+
+describe('FailureResponseObject', () => {
+  test('isSuccess should be false', () => {
+    const response = new FailureResponseObject();
+    expect(response.isSuccess()).toBeFalsy();
+  });
+
+  test('constructor takes type and message', () => {
+    const type = 'foo';
+    const message = 'bar';
+    const response = new FailureResponseObject(type, message);
+    expect(response.type).toBe(type);
+    expect(response.message).toBe(message);
+  });
+
+  test('buildSystemError should return object with type SYSTEM_ERROR', () => {
+    const message = 'test';
+    const response = FailureResponseObject.buildSystemError(message);
+    expect(response.type).toBe(FailureResponseObject.SYSTEM_ERROR);
+    expect(response.message).toBe(message);
+  });
+
+  test('buildValidationError should return object with type VALIDATION_ERROR', () => {
+    const message = 'test';
+    const response = FailureResponseObject.buildValidationError(message);
+    expect(response.type).toBe(FailureResponseObject.VALIDATION_ERROR);
+    expect(response.message).toBe(message);
+  });
+
+  test('buildFromInvalidRequest should build proper validation error', () => {
+    const request = new InvalidRequestObject();
+    request.addError('foo', 'bar');
+    request.addError('fizz', 'buzz');
+    const response = FailureResponseObject.buildFromInvalidRequestObject(request)
+    expect(response.type).toBe(FailureResponseObject.VALIDATION_ERROR);
+    expect(response.message).toEqual([
+      { parameter: 'foo', error: 'bar' },
+      { parameter: 'fizz', error: 'buzz' },
+    ]);
+  });
+
+});
